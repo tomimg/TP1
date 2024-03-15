414101c418b0632c5e9f0b46bd4db6afc5a091f9
tomimg <tomasmiranda466@gmail.com>
2024-03-14 22:56:03 -0300

    Changed the way search works

--- search.py
+++ search.py
@@ -1,8 +1,10 @@
-from load import datatemp
 import os
 import msvcrt
+bandera = False
 
-def search():
+def search(data):
+    global bandera
+    
     print('-------------------------')
     print('Bienvenido a la busqueda de personas!')
     print('-------------------------')
@@ -12,68 +14,48 @@ def search():
         print('-------------------------')
         print('Acabas de ingresar a la busqueda por DNI!')
         dni = int(input('Ingrese el DNI de la persona que desea buscar: '))
-        with open('data.txt', 'r') as fp:
-            for line in enumerate(fp):
-                if str(dni) in line:
-                    print('-------------------------')
-                    print('Hemos encontrado a tu persona:')
-                    print('-------------------------')
-                    print(line)
-                    print('-------------------------')
-                    print('Presione una tecla para continuar...')
-                    msvcrt.getch()
-                    os.system('cls')
-        fp.close()
-        for i in datatemp:
-            if str(dni) == i[3]:
+        for i, human in enumerate(data):
+            if human.dni == int(dni):
                 print('-------------------------')
                 print('Hemos encontrado a tu persona:')
                 print('-------------------------')
-                print('- Nombre:', i[0])
-                print('- Apellido:', i[1])
-                print('- Edad:', i[2])
-                print('- DNI:', i[3])
+                print('- Nombre:', human.name)
+                print('- Apellido:', human.surname)
+                print('- Edad:', human.age)
+                print('- DNI:', human.dni)
                 print('-------------------------')
                 print('Presione una tecla para continuar...')
                 msvcrt.getch()
                 os.system('cls')
-        print('No hemos encontrado a una persona con ese DNI.')
-        print('-------------------------')
-        print('Presione una tecla para continuar...')
-        msvcrt.getch()
-        os.system('cls')
+                bandera = True
+        if not bandera:
+            print('No hemos encontrado a una persona con ese DNI.')
+            print('-------------------------')
+            print('Presione una tecla para continuar...')
+            msvcrt.getch()
+            os.system('cls')
     else:
         os.system('cls')
         print('-------------------------')
         print('Acabas de ingresar a la busqueda por nombre!')
         nombre = str(input('Ingrese el nombre de la persona que desea buscar: '))
-        with open('data.txt', 'r') as file:
-            for line in enumerate(file):
-                if nombre.lower() in line:
-                    print('-------------------------')
-                    print('Hemos encontrado a tu persona:')
-                    print('-------------------------')
-                    print(line)
-                    print('-------------------------')
-                    print('Presione una tecla para continuar...')
-                    msvcrt.getch()
-                    os.system('cls')
-        file.close()
-        for i in datatemp:
-            if nombre.lower() == i[0]:
+        for i, human in enumerate(data):
+            if human.name.lower() == nombre.lower():
                 print('-------------------------')
-                print('Hemos encontrado a tu persona:')
+                print('Hemos encontrado a:')
                 print('-------------------------')
-                print('- Nombre:', i[0])
-                print('- Apellido:', i[1])
-                print('- Edad:', i[2])
-                print('- DNI:', i[3])
+                print('- Nombre:', human.name)
+                print('- Apellido:', human.surname)
+                print('- Edad:', human.age)
+                print('- DNI:', human.dni)
                 print('-------------------------')
                 print('Presione una tecla para continuar...')
                 msvcrt.getch()
                 os.system('cls')
-        print('No hemos encontrado a una persona con ese nombre.')
-        print('-------------------------')
-        print('Presione una tecla para continuar...')
-        msvcrt.getch()
-        os.system('cls')
+                bandera = True
+        if not bandera:
+            print('No hemos encontrado a una persona con ese nombre.')
+            print('-------------------------')
+            print('Presione una tecla para continuar...')
+            msvcrt.getch()
+            os.system('cls')
