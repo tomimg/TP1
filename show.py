c8fe0cb227e24006756076e96bf14b46e0698d36
tomimg <tomasmiranda466@gmail.com>
2024-03-14 22:57:25 -0300

    Changed the way data is printed

--- show.py
+++ show.py
@@ -1,26 +1,19 @@
-from load import datatemp
 import os
 import msvcrt
 
-def show():
+def show(data):
     file = open('data.txt', 'r')
     contador = 0
     print('-------------------------')
     print('La lista de personas es: ')
-    for line in file.readlines():
+    for i in data:
         contador += 1
         print('-------------------------')
-        print('Persona', contador)
-        print(line)
-    file.close()
-    for i in datatemp:
-        contador += 1
-        print('-------------------------')
-        print('Persona', contador)
-        print('- Nombre:', i[0])
-        print('- Apellido:', i[1])
-        print('- Edad:', i[2])
-        print('- DNI:', i[3])
+        print(f'Persona', contador)
+        print(f'- Nombre:', i.name)
+        print(f'- Apellido:', i.surname)
+        print(f'- Edad:', i.age)
+        print(f'- DNI:', i.dni)
     print('-------------------------')
     print('Presione una tecla para continuar...')
     msvcrt.getch()
