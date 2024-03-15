f0e7dcdf685d55e0c42ffd5cd37e05364e123ba0
tomimg <tomasmiranda466@gmail.com>
2024-03-14 22:59:32 -0300

    Change in the way of saving data

--- close.py
+++ close.py
@@ -1,15 +1,6 @@
-from load import datatemp
-
-def close():
-    file = open('data.txt', 'a')
-    for i in range(len(datatemp)):
-        for j in range(len(datatemp[i])):
-            if j == 0:
-                file.write("- Nombre: " + str(datatemp[i][j]) + ' ||')
-            elif j == 1:
-                file.write(" - Apellido: " + str(datatemp[i][j]) + ' ||')
-            elif j == 2:
-                file.write(" - Edad: " + str(datatemp[i][j]) + ' ||')
-            elif j == 3:
-                file.write(" - DNI: " + str(datatemp[i][j]) + ' ||' + '\n')
+from factory import Human
+def close(data):
+    file = open('data.txt', 'w')
+    for human in data:
+        file.write(f'{human.name},{human.surname},{human.age},{human.dni}\n')
     file.close()
